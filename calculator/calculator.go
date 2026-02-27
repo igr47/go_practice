bce777b6a9ad52f63f0fbb427f0cd333703c71f4
igr47 <njagielvis44@gmail.com>
2026-02-28 01:21:47 +0300

    Writting test and understanding them.

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -5,3 +5,11 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+func Subtract(a,b float64) float64 {
+	return a - b
+}
+
+func Multiply(a,b float64) float64 {
+	return a * b
+}
