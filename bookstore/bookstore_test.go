958c5650d6ef9d20249ff976f73ea3c74cc2707e
igr47 <njagielvis44@gmail.com>
2026-03-08 21:02:01 +0300

    Created a module names bookstore and create a test for it.

---
+++ bookstore/bookstore_test.go
@@ -0,0 +1,21 @@
+package bookstore_test
+
+import (
+	"bookstore"
+	"testing"
+)
+
+/*bookstore.Book{
+	Title: "Nicholas Chuckleby",
+	Author: "Charless Dickens",
+	Copies: 8,
+}*/
+
+func TestBook(t *testing.T) {
+	t.Parallel()
+	_=bookstore.Book{
+		Title: "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+}
